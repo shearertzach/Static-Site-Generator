3d1515615c321b8f53399026019ce698a7112e42
Zach Shearer <shearertzach@gmail.com>
2022-01-31 21:42:48 -0600

    Its working!!

--- makesite.go
+++ makesite.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"os"
+)
+
+type page struct {
+	TemplateFileName string
+	HTMLPagePath     string
+	Content          string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fileFlag := flag.String("file", "first-post.txt", "// The content the page will have. **MUST ADD .txt TO END of FILE NAME**")
+
+	fmt.Println(*fileFlag)
+
+	dat, _ := os.ReadFile(*fileFlag)
+	newPage := page{"template.tmpl", "first-post.html", string(dat)}
+
+	t := template.Must(template.New(newPage.TemplateFileName).ParseFiles(newPage.TemplateFileName))
+
+	newFile, err := os.Create(newPage.HTMLPagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	t.Execute(newFile, newPage)
 }
